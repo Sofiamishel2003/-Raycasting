aba53e9dbd5702e83abef0c8138423b0eb4d481f
sofiaWebT <98661058+Sofiamishel2003@users.noreply.github.com>
2024-08-12 21:48:13 -0600

    add method to detect a wall

--- src/maze.rs
+++ src/maze.rs
@@ -2,11 +2,20 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub fn load_maze(filename: &str) -> Vec<Vec<char>> {
-    let file = File::open(filename).unwrap();
+    let file = File::open(filename).expect(&format!("No se pudo abrir el archivo de laberinto: {}", filename));
     let reader = BufReader::new(file);
 
     reader
         .lines()
         .map(|line| line.unwrap().chars().collect())
         .collect()
+}
+
+pub fn is_wall(maze: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
+    let i = x / 100;
+    let j = y / 100;
+    if j >= maze.len() || i >= maze[j].len() {
+        return true;
+    }
+    maze[j][i] != ' '
 }
